c61b5c4d0095648d89f072d2ca9d96421efc6472
Qqi <vladislav.vaneev8@gmail.com>
2025-11-03 02:54:13 +0300

    KS-1-3 takeoff time, landing time

---
+++ app/src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -0,0 +1,21 @@
+package org.example.app.lesson_1
+
+fun main() {
+    val year: Int = 1961
+
+    //время взлёта
+    var hour: Int = 9
+    var minute: Int = 7
+
+    println(year)
+    println(hour)
+    println(minute)
+
+    //время посадки
+    hour = 10
+    minute = 55
+
+    print(hour)
+    print(":")
+    print(minute)
+}
