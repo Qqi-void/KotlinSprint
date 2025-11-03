5f6772ca33048bd1d735294605bc02b07e178430
Qqi <vladislav.vaneev8@gmail.com>
2025-11-03 13:43:27 +0300

    KS-1-5 renamed constants using UPPER_SNAKE_CASE

--- app/src/main/kotlin/lesson_1/lesson1_task5.kt
+++ app/src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,14 +1,14 @@
 package org.example.app.lesson_1
 
-const val secondsInHour = 3600
-const val secondsInMinute = 60
+const val SECONDS_IN_HOUR = 3600
+const val SECONDS_IN_MINUTE = 60
 
 fun main() {
     val totalSeconds: Int = 6480
 
-    val hours: Int = totalSeconds / secondsInHour
-    val minutes: Int = (totalSeconds % secondsInHour) / secondsInMinute
-    val seconds: Int = totalSeconds % secondsInMinute
+    val hours: Int = totalSeconds / SECONDS_IN_HOUR
+    val minutes: Int = (totalSeconds % SECONDS_IN_HOUR) / SECONDS_IN_MINUTE
+    val seconds: Int = totalSeconds % SECONDS_IN_MINUTE
 
     println(String.format("%02d:%02d:%02d", hours, minutes, seconds))
 }
