81c2f89f19368af402105df93ecc618eca6d97bd
Qqi <vladislav.vaneev8@gmail.com>
2025-11-03 13:35:51 +0300

    KS-1-5 added const val and String.format

--- app/src/main/kotlin/lesson_1/lesson1_task5.kt
+++ app/src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,20 +1,14 @@
 package org.example.app.lesson_1
 
+const val secondsInHour = 3600
+const val secondsInMinute = 60
+
 fun main() {
     val totalSeconds: Int = 6480
 
-    val hours: Int = totalSeconds / 3600
-    val minutes: Int = (totalSeconds % 3600) / 60
-    val seconds: Int = totalSeconds % 60
+    val hours: Int = totalSeconds / secondsInHour
+    val minutes: Int = (totalSeconds % secondsInHour) / secondsInMinute
+    val seconds: Int = totalSeconds % secondsInMinute
 
-    if (hours<10) print(0)
-    print(hours)
-    print(":")
-
-    if (minutes<10) print(0)
-    print(minutes)
-    print(":")
-
-    if (seconds<10) print(0)
-    print(seconds)
+    println(String.format("%02d:%02d:%02d", hours, minutes, seconds))
 }
