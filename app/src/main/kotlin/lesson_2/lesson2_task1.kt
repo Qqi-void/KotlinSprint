9c1486c20e001f9ad65b2a884f378d4322029d15
Qqi <vladislav.vaneev8@gmail.com>
2025-11-03 21:40:24 +0300

    KS-2-1 calculate the average grade

---
+++ app/src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -0,0 +1,11 @@
+package org.example.app.lesson_2
+
+fun main () {
+    val grade3 = 3
+    val grade4 = 4
+    val grade5 = 5
+    val count = 4
+
+    val average = (grade3+grade4+grade3+grade5).toFloat() / count
+    println(average)
+}
