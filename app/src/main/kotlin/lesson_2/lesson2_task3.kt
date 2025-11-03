2fb4435b30092b811d8848bd8f19aa7fea38f31a
Qqi <vladislav.vaneev8@gmail.com>
2025-11-03 21:49:43 +0300

    KS-2-3 train arrival time

---
+++ app/src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -0,0 +1,14 @@
+package org.example.app.lesson_2
+
+fun main() {
+    val departureHour = 9
+    val departureMinute = 39
+    val travelMinutes = 457
+    val minutesInHour = 60
+
+    val totalMinutes = departureHour * minutesInHour + departureMinute + travelMinutes
+    val arrivalHour = totalMinutes / minutesInHour
+    val arrivalMinute = totalMinutes % minutesInHour
+
+    println(String.format("%02d:%02d", arrivalHour, arrivalMinute))
+}
