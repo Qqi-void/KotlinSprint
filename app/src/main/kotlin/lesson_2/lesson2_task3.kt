9f0c1b743058b188ec4d2d29015ee1ce7469b0d9
Qqi <vladislav.vaneev8@gmail.com>
2025-11-04 18:05:26 +0300

    KS-2-3 added a constant

--- app/src/main/kotlin/lesson_2/lesson2_task3.kt
+++ app/src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -1,14 +1,15 @@
 package org.example.app.lesson_2
 
+const val MINUTES_IN_HOUR = 60
+
 fun main() {
     val departureHour = 9
     val departureMinute = 39
     val travelMinutes = 457
-    val minutesInHour = 60
 
-    val totalMinutes = departureHour * minutesInHour + departureMinute + travelMinutes
-    val arrivalHour = totalMinutes / minutesInHour
-    val arrivalMinute = totalMinutes % minutesInHour
+    val totalMinutes = departureHour * MINUTES_IN_HOUR + departureMinute + travelMinutes
+    val arrivalHour = totalMinutes / MINUTES_IN_HOUR
+    val arrivalMinute = totalMinutes % MINUTES_IN_HOUR
 
     println("%02d:%02d".format(arrivalHour, arrivalMinute))
 }
