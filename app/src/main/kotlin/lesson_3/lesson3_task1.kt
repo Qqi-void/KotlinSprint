72d963edbedd518204041c5b2ae37670078eeea1
Qqi <vladislav.vaneev8@gmail.com>
2025-11-04 14:53:45 +0300

    KS-3-1 greeting user based on time

---
+++ app/src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -0,0 +1,11 @@
+package org.example.app.lesson_3
+
+fun main() {
+    var greeting = "Good afternoon"
+    val userName = "Alex"
+
+    println("$greeting, $userName!")
+
+    greeting = "Good evening"
+    println("$greeting, $userName!")
+}
