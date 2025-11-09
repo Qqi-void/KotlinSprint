fbd8b2a767374f630be4e852a481d1410d2e01be
Qqi <vladislav.vaneev8@gmail.com>
2025-11-09 16:42:56 +0300

    KS-4-2 added const val

--- app/src/main/kotlin/lesson_4/lesson4_task3.kt
+++ app/src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -1,12 +1,15 @@
 package org.example.app.lesson_4
 
+const val IDEAL_HUMIDITY = 20
+const val SEASON_TO_AVOID = "winter"
+
 fun main() {
     val isSunny = true
     val isTentOpen = true
     val humidity = 20
     val season = "winter"
 
-    println("Благоприятные ли условия сейчас для роста бобовых? " +
-            (isSunny && isTentOpen && humidity == 20 && season != "winter")
-    )
+    val favorableConditions = isSunny && isTentOpen && humidity == IDEAL_HUMIDITY && season != SEASON_TO_AVOID
+
+    println("Благоприятные ли условия сейчас для роста бобовых? $favorableConditions")
 }
