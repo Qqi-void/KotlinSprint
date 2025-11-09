4a476449123e0872dd4f7a8534a0d21ea23c9cb4
Qqi <vladislav.vaneev8@gmail.com>
2025-11-09 16:21:50 +0300

    KS-4-1 added const val TOTAL_TABLES

--- app/src/main/kotlin/lesson_4/lesson4_task1.kt
+++ app/src/main/kotlin/lesson_4/lesson4_task1.kt
@@ -1,12 +1,14 @@
 package org.example.app.lesson_4
 
+const val TOTAL_TABLES = 13
+
 fun main() {
-    val totalTables = 13
+
     val bookedToday = 13
     val bookedTomorrow = 9
 
-    val availableToday = bookedToday < totalTables
-    val availableTomorrow = bookedTomorrow < totalTables
+    val availableToday = bookedToday < TOTAL_TABLES
+    val availableTomorrow = bookedTomorrow < TOTAL_TABLES
 
     println("Table availability for today: $availableToday")
     println("Table availability for tomorrow: $availableTomorrow")
