1706f52e5db2adaa50cabdbe6bbaf9f1aa6b9f8e
Qqi <vladislav.vaneev8@gmail.com>
2025-11-09 16:30:21 +0300

    KS-4-2 added const val

--- app/src/main/kotlin/lesson_4/lesson4_task2.kt
+++ app/src/main/kotlin/lesson_4/lesson4_task2.kt
@@ -1,19 +1,19 @@
 package org.example.app.lesson_4
 
-fun main() {
-    val minWeight = 35
-    val maxWeight = 100
-    val maxVolume = 100
+const val MIN_WEIGHT = 35
+const val MAX_WEIGHT = 100
+const val MAX_VOLUME = 100
 
+fun main() {
     val weight1 = 20
     val volume1 = 80
     println("Груз с весом $weight1 кг и объемом $volume1 л соответствует категории 'Average': " +
-            (weight1 > minWeight && weight1 <= maxWeight && volume1 < maxVolume)
+            (weight1 > MIN_WEIGHT && weight1 <= MAX_WEIGHT && volume1 < MAX_VOLUME)
     )
 
     val weight2 = 50
     val volume2 = 100
     println("Груз с весом $weight2 кг и объемом $volume2 л соответствует категории 'Average': " +
-            (weight2 > minWeight && weight2 <= maxWeight && volume2 < maxVolume)
+            (weight2 > MIN_WEIGHT && weight2 <= MAX_WEIGHT && volume2 < MAX_VOLUME)
     )
 }
