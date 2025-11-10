4f7bd150d2a291ef227e8704b373f657fb90e1f2
Qqi <vladislav.vaneev8@gmail.com>
2025-11-10 23:48:56 +0300

    KS-5-3 added const val

--- app/src/main/kotlin/lesson_5/lesson5_task3.kt
+++ app/src/main/kotlin/lesson_5/lesson5_task3.kt
@@ -1,15 +1,16 @@
 package org.example.app.lesson_5
 
+const val MIN_NUMBER = 0
+const val MAX_NUMBER = 42
+
 fun main() {
-    val minNumber = 0
-    val maxNumber = 42
     val winningNumber1 = 17
     val winningNumber2 = 40
 
-    print("Введите первое число (от $minNumber до $maxNumber): ")
+    print("Введите первое число (от $MIN_NUMBER до $MAX_NUMBER): ")
     val userNumber1 = readln().toInt()
 
-    print("Введите второе число (от $minNumber до $maxNumber): ")
+    print("Введите второе число (от $MIN_NUMBER до $MAX_NUMBER): ")
     val userNumber2 = readln().toInt()
 
     val resultText = when {
