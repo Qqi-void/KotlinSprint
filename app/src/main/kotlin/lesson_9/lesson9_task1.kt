fda6f24f8c877c9f8ed64f9d0790aac5fb637e4a
Qqi <vladislav.vaneev8@gmail.com>
2026-01-04 21:55:50 +0300

    KS-9-1 Print ingredients using List.

---
+++ app/src/main/kotlin/lesson_9/lesson9_task1.kt
@@ -0,0 +1,9 @@
+package org.example.app.lesson_9
+
+fun main() {
+    val ingredients = listOf("potatoes", "peas", "cucumbers", "chicken", "onion", "eggs")
+
+    println("В рецепте есть следующие ингредиенты: $ingredients")
+
+    ingredients.forEach { println(it) }
+}
